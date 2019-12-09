c23a2179d74df4194b565d95ff96bd239caa7e0c
fmmachado10 <contato.fabricio@gmail.com>
2019-12-09 09:36:29 -0300

    Adicionando novas páginas

--- produto.py
+++ produto.py
@@ -12,32 +12,46 @@ app = Flask(__name__)
 
 
 class Produto:
-    def __init__(self, nome, quantidade, valor):
+    def __init__(self, nome, quantidade, valor, id):
+        self.id = id
         self.nome = nome
         self.quantidade = quantidade
         self.valor = valor
 
 
-produto1 = Produto('Produto Inicial 01', '10', '10,00')
-produto2 = Produto('Produto Inicial 02', '10', '20,00')
-lista = [produto1, produto2]
+#produto1 = Produto('Produto Inicial 01', '10', '10,00', 0)
+#produto2 = Produto('Produto Inicial 02', '10', '20,00', 0)
+lista = []
 
 
 @app.route('/')
-def index():
-    return render_template('lista.html', titulo='produtos', produtos=lista)
+def index():    
 
+    lista = []
 
-@app.route('/deletar/<int:id>')
-def deletar(id):
+    documents = collection.find()
     
-    collection.delete_one(id)
-    return render_template('novo.html', titulo='Produto', produtos=lista)  
+    for document in documents:
 
+        p = Produto(str(document['nome']), str(document['quantidade']), str(document['valor']), str(document['_id']))
+        
+        lista.append(p)            
+    
+    return render_template('lista.html', titulo='produtos', produtos=lista)
 
 @app.route('/novo')
 def novo():
-    return render_template('novo.html', titulo='Novo Produto')
+
+    lista = []
+
+    documents = collection.find()
+    
+    for document in documents:
+        p = Produto(str(document['nome']), str(document['quantidade']), str(document['valor']), str(document['_id']))
+        
+        lista.append(p)
+
+    return render_template('novo.html', titulo='Novo Produto', produtos=lista)
 
 
 @app.route('/criar', methods=['POST',])
@@ -45,14 +59,65 @@ def criar():
     nome = request. form['nome']
     quantidade = request. form['quantidade']
     valor = request. form['valor']
-    produto = Produto(nome, quantidade, valor)
-    lista.append(produto)
+    #produto = Produto(nome, quantidade, valor, str(id))
+    #lista.append(produto)
 
     prod = {"nome": nome, "quantidade": quantidade, "valor": valor }
 
     collection.insert_one(prod)
 
+    lista = []
+
+    documents = collection.find()
+    
+    for document in documents:
+        p = Produto(str(document['nome']), str(document['quantidade']), str(document['valor']), str(document['_id']))
+        
+        lista.append(p)
+
+
+
+
     return render_template('novo.html', titulo='Produto', produtos=lista)
+
+#------------------------EDITAR-----------------------------------
+@app.route('/editar/<id>')
+def editar(id):
+
+    print("ID para ser atualizado: " + id)
+    
+    p = collection.find_one({"_id":id})
+
+    produto = Produto(str(p['nome']), str(p['quantidade']), str(p['valor']), str(p['_id']))
+
+
+    return render_template('editar.html', titulo='Editando Produto', produto=produto )
+
+
+@app.route('/atualizar', methods=['POST',])
+def atualizar():
+  pass
+#---------------------------------------------
+
+@app.route('/deletar')
+def deletar(id):
+
+    print('aki01...')
+
+    collection.delete_one(id)
+
+    return render_template('novo.html', titulo='Produto', produtos=lista) 
+
+
+
+
+@app.route('/limpar')
+def limpar():
+
+    print('aki01...')
+
+    return render_template('lista.html', titulo='produtos', produtos=lista) 
+
 
 
 app.run(debug=True)
